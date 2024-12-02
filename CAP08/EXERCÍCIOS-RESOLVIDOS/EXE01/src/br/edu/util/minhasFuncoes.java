eb8f332f9fe89588f07e9515dcf5e6d2296a2ad7
Leandro Brasil <leandro.brasil09@aluno.ifce.edu.br>
2024-12-02 14:06:11 -0300

    Add files via upload

---
+++ CAP08/EXERCÍCIOS-RESOLVIDOS/EXE01/src/br/edu/util/minhasFuncoes.java
@@ -0,0 +1,17 @@
+package br.edu.util;
+public class minhasFuncoes {	
+	public static double ver(double numero) {
+			int RES=0;
+			if (numero >= 0) {
+				RES = 1;
+				return RES;
+			}
+			else {
+				return RES;
+			}
+		}
+
+	}
+	
+
+
